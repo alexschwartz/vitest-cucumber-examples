1acf2f2840831e1f2ff1f82b94a43f7bf0e97416
Alex Schwartz <alexschwartz@users.noreply.github.com>
2024-09-24 17:00:15 +0200

    given works

--- example_standalone/src/__tests__/example-feature.spec.ts
+++ example_standalone/src/__tests__/example-feature.spec.ts
@@ -5,9 +5,5 @@ const feature = await loadFeature('features/example1.feature')
 describeFeature(feature, ({ Scenario }) => {
     Scenario('invoke algorithm', ({ Given, When, Then, And }) => {
         Given(`the keyword {string}`, (keyword:string) => {})
-        And('I have a feature like "example.feature"', () => {})
-        When('I run vitest-cucumber', () => {})
-        Then('My feature file is parsed', () => {})
-        And('I can test my scenarios', () => {})
     })
 })
