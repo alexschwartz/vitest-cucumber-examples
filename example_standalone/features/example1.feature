43cc83cff8c219b2464e69b7f19bd0fa9ad8f18c
Alex Schwartz <alexschwartz@users.noreply.github.com>
2024-09-24 16:59:09 +0200

    next try

--- example_standalone/features/example1.feature
+++ example_standalone/features/example1.feature
@@ -1,5 +1,5 @@
 Feature: use vitest-cucumber to test an algorithm
     Scenario: invoke algorithm
-        Given the keywork "nice"
+        Given a keyword "nice"
         When  the algorithm is invoked with the keyword
         Then  the result is "very nice"
--- example_standalone/src/__tests__/example-feature.spec.ts
+++ example_standalone/src/__tests__/example-feature.spec.ts
@@ -3,8 +3,8 @@ import { loadFeature, describeFeature } from '@amiceli/vitest-cucumber'
 const feature = await loadFeature('features/example1.feature')
 
 describeFeature(feature, ({ Scenario }) => {
-    Scenario('use vitest-cucumber to test an algorithm', ({ Given, When, Then, And }) => {
-        Given('I have installed vitest-cucumber', () => {})
+    Scenario('invoke algorithm', ({ Given, When, Then, And }) => {
+        Given(`the keyword {string}`, (keyword:string) => {})
         And('I have a feature like "example.feature"', () => {})
         When('I run vitest-cucumber', () => {})
         Then('My feature file is parsed', () => {})
