28292a9c25d364028cf6b38c7c42d3fa49a82fe2
Alex Schwartz <alexschwartz@users.noreply.github.com>
2024-09-24 17:01:50 +0200

    now passing without assering anything

--- example_standalone/src/__tests__/example-feature.spec.ts
+++ example_standalone/src/__tests__/example-feature.spec.ts
@@ -5,5 +5,7 @@ const feature = await loadFeature('features/example1.feature')
 describeFeature(feature, ({ Scenario }) => {
     Scenario('invoke algorithm', ({ Given, When, Then, And }) => {
         Given(`the keyword {string}`, (keyword:string) => {})
+        When('the algorithm is invoked with the keyword', () => {})
+        Then(`the result is {string}`, (keyword:string) => {})
     })
 })
